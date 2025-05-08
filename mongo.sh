42a406be1f2ab134ab74c73bd7549baa6ac3ef4e
Rohan <rohangupta1996>
2025-05-08 17:36:42 +0530

    Frontend changes

--- mongo.sh
+++ mongo.sh
@@ -7,7 +7,7 @@ print_head Install MongoDB
 dnf install mongodb-org -y
 
 print_head Update mongoDB Config file
-sed -i -e 's|127.0.0.0|0.0.0.0|' /etc/mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 
 print_head Start MongoDB service
 systemctl enable mongod &>> $log_file
