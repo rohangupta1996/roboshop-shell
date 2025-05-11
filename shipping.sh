cb7d40866af51454fbc4e4fb5b213d337e258aa6
Rohan <rohangupta1996>
2025-05-11 16:17:00 +0530

    Frontend changes

--- shipping.sh
+++ shipping.sh
@@ -9,10 +9,10 @@ print_head Install MySQL Client
 dnf install mysql -y &>>$log_file
 
 print_head Load Schema
-mysql -h mysql-dev.rdevopsb83.online -uroot -pRoboShop@1 < /app/db/schema.sql &>>$log_file
+mysql -h mysql-dev.ro-bot.store -uroot -pRoboShop@1 < /app/db/schema.sql &>>$log_file
 
 print_head Load User creation
-mysql -h mysql-dev.rdevopsb83.online -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$log_file
+mysql -h mysql-dev.ro-bot.store -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$log_file
 
 print_head Load Master Data
-mysql -h mysql-dev.rdevopsb83.online -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
+mysql -h mysql-dev.ro-bot.store -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
